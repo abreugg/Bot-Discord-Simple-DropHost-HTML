e333949000ef70e01709c30d86e23d1177d90341
Juan Abreu <contatojuanabreu@gmail.com>
2024-04-04 18:29:12 -0300

    algumas paradas ai

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const client = new Discord.Client({
 });
 const config = require("./config.json");
 
-function executeCommand(command) {
+function executeCommand(command, message) {
     return new Promise((resolve, reject) => {
         exec(command, (error, stdout, stderr) => {
             if (error) {
@@ -16,6 +16,9 @@ function executeCommand(command) {
             } else {
                 console.log(`Comando executado com sucesso: ${command}`);
                 console.log(stdout);
+                if(stdout){
+                    message.channel.send(stdout.replace(/\/var\/www\/html\//g, "https://"));
+                }
                 resolve();
             }
         });
@@ -26,16 +29,28 @@ client.on("messageCreate", async (message) => {
     if (message.author.bot) return;
     if (message.channel.type === Discord.ChannelType.DM) return;
     if (message.channel.id == config.canal && message.attachments) {
-        message.attachments.map(async file => {
-            if (file.contentType !== 'application/zip') return message.reply('O arquivo deve esta em formato ZIP')
-            await executeCommand(`curl -o /var/www/html/${config.domain}/${message.author.username}/${file.name} ${file.url}`);
-            await executeCommand(`unzip -o /var/www/html/${config.domain}/${message.author.username}/${file.name} -d /var/www/html/${config.domain}/${message.author.username}`);
-            await executeCommand(`rm /var/www/html/${config.domain}/${message.author.username}/${file.name}`);
-            message.channel.send(`${file.name} upado com sucesso!\nURL: https://${config.domain}/${message.author.username}`)
-        })
-    }
+        for (const file of message.attachments.values()) {
+            if (file.contentType !== 'application/zip') return message.reply('O arquivo deve estar em formato ZIP');
+            
+            try {
+                await executeCommand(`curl -o /var/www/html/${config.domain}/${message.author.username}/${file.name} '${file.url}'`, message);
+                console.log(`Download do arquivo ${file.name} concluído`);
+                
+                await executeCommand(`unzip -o /var/www/html/${config.domain}/${message.author.username}/${file.name} -d /var/www/html/${config.domain}/${message.author.username}`, message);
+                console.log(`Descompactação do arquivo ${file.name} concluída`);
+                
+                await executeCommand(`rm /var/www/html/${config.domain}/${message.author.username}/${file.name}`, message);
+                console.log(`Remoção do arquivo ${file.name} concluída`);
 
+                message.channel.send(`${file.name} upado com sucesso!\nURL: https://${config.domain}/${message.author.username}`);
+            } catch (error) {
+                console.error(`Erro ao processar o arquivo ${file.name}: ${error}`);
+                message.channel.send(`Ocorreu um erro ao processar o arquivo ${file.name}.`);
+            }
+        }
+    }
 });
+
 
 
 client.on("ready", () => {
